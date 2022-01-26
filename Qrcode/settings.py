1469f5cd4cf6396b01a66523b472a198364c65fe
itsankushsingh <96742942+itsankushsingh@users.noreply.github.com>
2022-01-26 22:40:17 +0530

    Update settings.py

--- Qrcode/settings.py
+++ Qrcode/settings.py
@@ -124,4 +124,5 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
